e396a53e1d42a7604e2d4fc52cf3afdd4bca0b08
靳炳淑 <jinbingshu_nju@qq.com>
2018-10-14 23:53:48 +0800

    get started

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="header-main">
+        <div class="header-body">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/config";
+.header-main {
+    display: flex;
+    justify-items: center;
+    flex-direction: column;
+    .header-body {
+        .header-left {
+            .header-link-text {
+
+            }
+        }
+        .header-right {
+            .header-icon {
+
+            }
+        }
+    }
+}
+
+</style>
